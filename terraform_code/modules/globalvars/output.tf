b1a8b907ef160cd1ef4b12db85b253fda194e4d1
EC2 Default User <ec2-user@ip-172-31-86-198.ec2.internal>
2023-02-18 21:45:35 +0000

    first commit

---
+++ terraform_code/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags for web
+output "default_tags" {
+  value = {
+    "Owner" = "Nubila"
+    "App"   = "Web"
+    "Project" = "CLO835"
+  }
+}
+
+# Prefix to use
+output "prefix" {
+  value     = "assignment1"
+}
